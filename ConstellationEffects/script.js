27d811eedca8877d80a2646b422e97f341eb3c03
Muftawo <hajjmuf@gmail.com>
2023-11-18 10:10:34 +0000

    added general parameters obj

--- ConstellationEffects/script.js
+++ ConstellationEffects/script.js
@@ -3,12 +3,12 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-console.log(ctx);
-console.log(canvas);
-const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
-gradient.addColorStop(0, 'white');
-gradient.addColorStop(0.5, 'magenta');
-gradient.addColorStop(1, 'blue');
+parameters ={
+    numberOfParticles: 400,
+    particleConnectionMaxDistance : 100,
+
+}
+
 
 ctx.fillStyle = gradient;
 ctx.strokeStyle = 'white';
