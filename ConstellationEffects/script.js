b9d51a1813c453cb3befef83486dd7f1e79a62dc
Muftawo <hajjmuf@gmail.com>
2023-11-18 11:51:23 +0000

    added function for particle collision with frame

--- ConstellationEffects/script.js
+++ ConstellationEffects/script.js
@@ -85,15 +85,27 @@ class Particle {
             }
         }
     }
+
+    onFrameCollision() {
+        //horizontal collision
+        if (this.x < this.radius) {
+            this.x = this.radius;
             this.vx *= -1;
-            this.changeRadius();
+        } else if (this.x > this.effect.width - this.radius) {
+            this.x = this.effect.width - this.radius;
+            this.vx *= -1;
         }
 
-        this.y += this.vy;
-        if (this.y > this.effect.height - this.radius || this.y < this.radius) {
+        //vertical collision
+        if (this.y < this.radius) {
+            this.y = this.radius;
             this.vy *= -1;
-            this.changeRadius();
+        } else if (this.y > this.effect.height - this.radius) {
+            this.y = this.effect.height - this.radius;
+            this.vy *= -1;
         }
+    }
+
     }
 }
 
